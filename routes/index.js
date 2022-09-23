d280d20480bcec881d14506415a9de68f4562c94
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2022-09-22 23:38:07 -0700

    added switch cases to inquirer menu questions

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 
 const app = express();
 
-// Connect to database
+// **************** Connect to database **************** //
 const db = mysql.createConnection(
     {
       host: 'localhost',
@@ -14,6 +14,39 @@ const db = mysql.createConnection(
     },
     console.log(`Connected to the movies_db database.`)
 );
+
+// **************** Read Routes **************** //
+// Read all departments
+app.get('/departments', (req, res) => {
+  const sql = `SELECT * FROM department`;
+  
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+       return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
+
+// Read all roles
+app.get('/roles', (req, res) => {
+  const sql = `SELECT * FROM role`;
+  
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+       return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
 
 // Read all employees
 app.get('/employees', (req, res) => {
